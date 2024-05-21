5a142bbf57cabe9a73e540c54852fe85e43c1704
CP21H <cp21h@fsu.edu>
2024-05-21 11:52:51 -0400

    main(), setKeys(), encryptMSG(), decryptMSG(), and decode() Implemented

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 std::vector<int> primeStorage;
 int PUBLIC_KEY;
 int PRIVATE_KEY;
+int N;
 
 /*
  *   //- FUNCTION DECLARATIONS
@@ -18,20 +19,27 @@ int PRIVATE_KEY;
 void fillPrimes();
 int selectPrime();
 void setKeys();
-long encryptMSG(double message);
+long long int encryptMSG(double message);
+long long int decryptMSG(int encryptedMSG);
 std::vector<int> encode(std::string message);
+std::string decode(std::vector<int> encodedMSG);
 
 /*
  *   //- MAIN
  * */
 int main() {
+    fillPrimes();
+    setKeys();
     std::string message;
     std::cout << "Enter Initial Message: ";
-    std::cin >> message;
+    std::getline(std::cin, message);
     std::cout << "\nInitial Message: " << message << std::endl;
-    // rsa encrypt
-    // print encrypted message
-    // print decrypted message
+    std::vector<int> coded = encode(message);
+    std::cout << "\nEncoded Message: ";
+    for (int i = 0; i < coded.size(); i++) {
+        std::cout << coded[i];
+    }
+    std::cout << "\n\nDecoded Message: " << decode(coded) << std::endl;
 
     return 0;
 }
@@ -85,7 +93,29 @@ int selectPrime() {
 // * References: GeeksForGeeks RSA-Algorithm-Cryptography          *
 // *****************************************************************
 void setKeys() {
+    int p = selectPrime();
+    int q = selectPrime();
+    N = p * q;
+    int lambda = std::lcm(p-1, q-1);
 
+    int e = 2;
+    while (1) {
+        if (std::gcd(e, lambda) == 1) {
+            break;
+        }
+        e++;
+    }
+    PUBLIC_KEY = e;
+
+    int d = 2;
+    while (1) {
+        // Private key is not simply e % lambda, but d * e % lambda
+        if ((d * e) % lambda == 1) {
+            break;
+        }
+        d++;
+    }
+    PRIVATE_KEY = d;
 }
 
 // *****************************************************************
@@ -96,8 +126,32 @@ void setKeys() {
 // * Author: Cristhian Prado                                       *
 // * References: GeeksForGeeks RSA-Algorithm-Cryptography          *
 // *****************************************************************
-long encryptMSG(double message) {
+long long int encryptMSG(double message) {
+    int e = PUBLIC_KEY;
+    long long int encryptedMSG = 1;
+    while (e--) {
+        encryptedMSG *= message;
+        encryptedMSG %= N;
+    }
+    return encryptedMSG;
+}
 
+// *****************************************************************
+// * Function Name: decryptMSG(int encryptedMSG)                   *
+// * Description: Decrypts message and returns it as a long        *
+// * Parameter Description: Encrypted message as an int            *
+// * Date: 05/21/2024                                              *
+// * Author: Cristhian Prado                                       *
+// * References: GeeksForGeeks RSA-Algorithm-Cryptography          *
+// *****************************************************************
+long long int decryptMSG(int encryptedMSG) {
+    int d = PRIVATE_KEY;
+    long long int decryptedMSG = 1;
+    while (d--) {
+        decryptedMSG *= encryptedMSG;
+        decryptedMSG %= N;
+    }
+    return decryptedMSG;
 }
 
 // *****************************************************************
@@ -116,4 +170,20 @@ std::vector<int> encode(std::string message) {
     }
 
     return encodedMSG;
+}
+
+// *****************************************************************
+// * Function Name: decode(std::vector<int> encodedMSG)            *
+// * Description: Decodes vector encoded message into string       *
+// * Parameter Description: Encoded vector message passed in       *
+// * Date: 05/21/2024                                              *
+// * Author: Cristhian Prado                                       *
+// * References: GeeksForGeeks RSA-Algorithm-Cryptography          *
+// *****************************************************************
+std::string decode(std::vector<int> encodedMSG) {
+    std::string s;
+    for (int i = 0; i < encodedMSG.size(); i++) {
+        s += decryptMSG(encodedMSG[i]);
+    }
+    return s;
 }
