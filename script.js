787d16c342fea2b7dee2c490c39ab8da57deae42
CP21H <cp21h@fsu.edu>
2024-05-23 12:45:52 -0400

    Website Conversion Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function submRun() {
+    let post_text = document.getElementById("encryptedMSG");
+    let input = document.getElementById("initMSG");
+    //let encryptedMSG = 
+
+    //post_text.innerHTML = 
+}
+
+// functions for RSA implementation below
